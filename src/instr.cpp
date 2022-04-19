574194dd60fd3a7f5dd33f03d5f87be243cb0e92
Patrik Nemeth <patriknemethpn@gmail.com>
2022-04-19 19:47:47 +0200

    Beauty issues

--- src/instr.cpp
+++ src/instr.cpp
@@ -13,7 +13,7 @@ Stats measure_mov(uint64_t src, volatile uint64_t *mem, Measurement *m, uint64_t
    uint64_t added_cnt = 0;
    while (added_cnt != runs) {
         m->start_measurement();
-        for (uint64_t i = 0; i < iterations; ++i) {
+        for (uint64_t i = 0; i < iterations; i++) {
             asm volatile ("mov %0, %1" : "=r"(dst) : "m"(mem[i]));
         }
         m->stop_measurement();
@@ -36,7 +36,7 @@ Stats measure_xor(volatile uint64_t op, Measurement *m, uint64_t runs, uint64_t 
    uint64_t added_cnt = 0;
    while (added_cnt != runs) {
         m->start_measurement();
-        for (uint64_t i = 0; i < iterations; ++i) {
+        for (uint64_t i = 0; i < iterations; i++) {
             asm volatile ("xor %0, %1" : "=r"(a) : "r"(op));
         }
         m->stop_measurement();
@@ -59,7 +59,7 @@ Stats measure_inc(Measurement *m, uint64_t runs, uint64_t iterations)
    uint64_t added_cnt = 0;
    while (added_cnt != runs) {
         m->start_measurement();
-        for (uint64_t i = 0; i < iterations; ++i) {
+        for (uint64_t i = 0; i < iterations; i++) {
             asm volatile ("inc %0" : "=r"(a));
         }
         m->stop_measurement();
@@ -82,7 +82,7 @@ Stats measure_imul(volatile uint64_t op, Measurement *m, uint64_t runs, uint64_t
     uint64_t added_cnt = 0;
     while (added_cnt != runs) {
         m->start_measurement();
-        for (uint64_t i = 0; i < iterations; ++i) {
+        for (uint64_t i = 0; i < iterations; i++) {
             asm volatile ("imul $0x8, %1" : "=r"(a) : "r"(op));
         }
         m->stop_measurement();
@@ -107,7 +107,7 @@ Stats measure_rdrand(Measurement *m, uint64_t runs, uint64_t iterations)
    uint64_t added_cnt = 0;
    while (added_cnt != runs) {
         m->start_measurement();
-        for (uint64_t i = 0; i < iterations; ++i) {
+        for (uint64_t i = 0; i < iterations; i++) {
             asm volatile ("rdrand %0" : "=r"(a));
         }
         m->stop_measurement();
@@ -132,7 +132,7 @@ Stats measure_fscale(Measurement *m, uint64_t runs, uint64_t iterations)
    uint64_t added_cnt = 0;
    while (added_cnt != runs) {
         m->start_measurement();
-        for (uint64_t i = 0; i < iterations; ++i) {
+        for (uint64_t i = 0; i < iterations; i++) {
             asm volatile ("fscale");
         }
         m->stop_measurement();
@@ -159,7 +159,7 @@ Stats measure_rdtsc(Measurement *m, uint64_t runs, uint64_t iterations)
    uint64_t added_cnt = 0;
    while (added_cnt != runs) {
         m->start_measurement();
-        for (uint64_t i = 0; i < iterations; ++i) {
+        for (uint64_t i = 0; i < iterations; i++) {
             asm volatile ("rdtsc" : "=a" (a), "=d" (d));
         }
         m->stop_measurement();
@@ -186,7 +186,7 @@ Stats measure_clflush(Measurement *m, uint64_t runs, uint64_t iterations)
    uint64_t added_cnt = 0;
    while (added_cnt != runs) {
         m->start_measurement();
-        for (uint64_t i = 0; i < iterations; ++i) {
+        for (uint64_t i = 0; i < iterations; i++) {
             asm volatile ("clflush (%0)" :: "r"(&a));
         }
         m->stop_measurement();
@@ -209,7 +209,7 @@ Stats measure_nop(Measurement *m, uint64_t runs, uint64_t iterations)
    uint64_t added_cnt = 0;
    while (added_cnt != runs) {
         m->start_measurement();
-        for (uint64_t i = 0; i < iterations; ++i) {
+        for (uint64_t i = 0; i < iterations; i++) {
             asm volatile ("nop");
         }
         m->stop_measurement();
